e9cf8cc1fa0b2a2a27a0621577eeedd49d879d35
louwailou <jianfeng224@gmail.com>
2015-05-29 14:48:54 +0800

    Initial Commit

---
+++ Rotate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotate
+//
+//  Created by Sun on 15/5/29.
+//  Copyright (c) 2015年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotate
+//
+//  Created by Sun on 15/5/29.
+//  Copyright (c) 2015年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
